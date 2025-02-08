933d16c8fe238732df4cd2d67906d473079f6cc7
kimhao97 <nguyenkimhaodn@gmail.com>
2025-02-08 11:49:42 +0700

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-const Card = ({title}) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]); // Chỉ chạy khi hasLiked thay đổi
-
-  return (
-    <div className='card' onClick={() => setCount(count + 1)}>
-      <h2>{title} <br/> {count || null} </h2>
-      
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "🩷" : "🤍"}
-      </button>
+const App = () => {
+  return <div>
+      <h1 class="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
     </div>
-  )
-}
-function App() {
-  return newFunction()
-
-  function newFunction() {
-    return <div className='card-container'>
-      <h2>Card</h2>
-      <Card title="Box A" />
-      <Card title="Box B" />
-      <Card title="Box C" />
-    </div>
-  }
-}
+};
 
 export default App

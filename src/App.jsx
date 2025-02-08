59b21f84215ef1d6ca1e867ed5e52662726946e6
kimhao97 <nguyenkimhaodn@gmail.com>
2025-02-08 16:35:12 +0700

    create assets and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const App = () => {
-  return <div>
-      <h1 class="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+  return (
+    <div className='wrapper'>
+      <header>
+        <img src="./hero.png" alt="Hero Banner"/> 
+        <h1>Find <span className='text-gradient'>Movies</span>You'll Enjoy Without the Hassle</h1>
+      </header>
+      <p>Search</p>
     </div>
-};
+  )
+}
 
 export default App
